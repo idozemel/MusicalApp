7ff70f96474f2014e9d27533869fd2c0f12cb542
Dudi Samarel <dudihls@gmail.com>
2022-08-21 23:32:06 +0300

    delete song

--- backend/modules/song/song.routes.ts
+++ backend/modules/song/song.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { requireAdmin } from "../../middlewares/auth";
-import { getSong, getSongs, saveSong } from "./song.controller";
+import { getSong, getSongs, saveSong, deleteSong } from "./song.controller";
 const songRouter = Router();
 
 songRouter.post("/", requireAdmin, saveSong);
 songRouter.put("/:id", requireAdmin, saveSong);
 songRouter.get("/", getSongs);
 songRouter.get("/:id", getSong);
+songRouter.delete("/:id", requireAdmin, deleteSong);
 
 export default songRouter;
